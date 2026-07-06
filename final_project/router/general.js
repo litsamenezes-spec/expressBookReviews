cf11910764efe6ee5202f91f98f157590ea7b1bb
LitsaMenezes <litsamenezes@gmail.com>
2026-07-06 17:52:13 +0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,44 +3,41 @@ const axios = require('axios');
 
 const public_users = express.Router();
 
-const baseURL = "http://localhost:5000"; 
+const baseURL = "http://localhost:5000";
 
-public_users.get('/', async function (req, res) {
+public_users.get('/', async (req, res) => {
     try {
         const response = await axios.get(`${baseURL}/`);
-        return res.status(200).send(JSON.stringify(response.data, null, 4));
+        res.status(200).json(response.data);
     } catch (error) {
-        return res.status(500).json({ message: "Error fetching books" });
+        res.status(500).json({ message: "Error fetching books" });
     }
 });
 
-public_users.get('/isbn/:isbn', async function (req, res) {
+public_users.get('/isbn/:isbn', async (req, res) => {
     try {
-        const isbn = req.params.isbn;
-        const response = await axios.get(`${baseURL}/isbn/${isbn}`);
-        return res.status(200).json(response.data);
+        const response = await axios.get(`${baseURL}/isbn/${req.params.isbn}`);
+        res.status(200).json(response.data);
     } catch (error) {
-        return res.status(500).json({ message: "Error fetching book by ISBN" });
+        res.status(500).json({ message: "Error fetching book by ISBN" });
     }
 });
 
-public_users.get('/author/:author', async function (req, res) {
+public_users.get('/author/:author', async (req, res) => {
     try {
-        const author = req.params.author;
-        const response = await axios.get(`${baseURL}/author/${author}`);
-        return res.status(200).json(response.data);
+        const response = await axios.get(`${baseURL}/author/${req.params.author}`);
+        res.status(200).json(response.data);
     } catch (error) {
-        return res.status(500).json({ message: "Error fetching books by author" });
+        res.status(500).json({ message: "Error fetching books by author" });
     }
 });
 
-public_users.get('/title/:title', async function (req, res) {
+public_users.get('/title/:title', async (req, res) => {
     try {
-        const title = req.params.title;
-        const response = await axios.get(`${baseURL}/title/${title}`);
-        return res.status(200).json(response.data);
+        const response = await axios.get(`${baseURL}/title/${req.params.title}`);
+        res.status(200).json(response.data);
     } catch (error) {
-        return res.status(500).json({ message: "Error fetching books by title" });
+        res.status(500).json({ message: "Error fetching books by title" });
     }
 });
 
